89a134b37918d6f8061113dc11bfa50956bfa7d3
Dimitris Dimitropoulos <ddimitrop@gmail.com>
2024-06-21 12:44:07 -0700

    Addind docker container building

---
+++ scripts/docker-run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+version=$1
+environment=$2
+
+if [ ! $version ]
+then 
+  version=1.0
+fi
+
+if [ ! $environment ]
+then 
+  environment=dev
+fi
+
+docker stop hoally-$environment 2>/dev/null
+docker rm hoally-$environment 2>/dev/null
+docker run -d -p 8080:8080 --name  hoally-$environment hoally:$version
